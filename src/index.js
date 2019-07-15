131087fda23caf660404e6245164eddf7c37b12b
junaidshad <xadxunaid@gmail.com>
2019-07-15 21:43:02 +0500

    Completed Section 3(Communicating with the props).

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    return (
+      <div className="ui container comments">
+        <ApprovalCard>
+          <div>
+            <h4>Warning!</h4>
+            Are you sure you want to do this?
+          </div>
+        </ApprovalCard>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        <ApprovalCard>
+          <CommentDetail author="Sam" timeAgo="Today at 4:45PM" comment="Great Developer" avatar={faker.image.avatar()}/>
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail author="Edwardo" timeAgo="Today at 3:45AM" comment="I am pleased to collaborate with you" avatar={faker.image.avatar()}/>
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail author="Ericka" timeAgo="Today at 2:00PM" comment="A man with nice sense of humor" avatar={faker.image.avatar()}/>
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail author="Davon" timeAgo="Today at 9:00AM" comment="Recommended for consultation" avatar={faker.image.avatar()}/>
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail author="Albina" timeAgo="Today at 4:00PM" comment="I'm just speechless" avatar={faker.image.avatar()}/>
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail author="Tanya" timeAgo="Today at 5:35PM" comment="Great platform for collaborations" avatar={faker.image.avatar()}/>
+        </ApprovalCard>
+      </div>
+    );
+};
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
